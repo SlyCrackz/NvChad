ab138bea6db74c5357b0363cf3f5456f334616ba
slycrackz <slycrackz@slycrackz.xyz>
2024-09-17 03:53:35 -0400

    added nil_ls a nix language server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "zls" }
+local servers = { "html", "cssls", "clangd", "zls", "nil_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
